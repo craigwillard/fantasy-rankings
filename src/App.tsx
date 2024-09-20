5b43f0bc525b059fa14cdb8db104d3974526a029
Craig Willard <craigwillard@gmail.com>
2024-09-20 09:58:37 -0400

    Switch from useState to useReducer. Fix dark mode to go fullscreen.

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,85 @@
 import "./App.scss";
 
-import { createContext, useState } from "react";
+import { useReducer } from "react";
 import Header from "./components/header/header";
 import PlayerList from "./components/playerList/playerList";
 import PositionSelect from "./components/positionSelect/positionSelect";
 import { searchablePositions } from "./data/searchablePositions";
 
-const ModeContext = createContext<string>("");
+type AppState = {
+  position: string;
+  searchTerm: string;
+  mode: string;
+};
+
+function reducer(state: AppState, action: any) {
+  const { type, payload } = action;
+  switch (type) {
+    case "updatePosition":
+      return { ...state, position: payload };
+    case "updateSearchTerm":
+      return { ...state, searchTerm: payload };
+    case "updateMode":
+      return { ...state, mode: payload };
+    default:
+      throw new Error("No action provided");
+  }
+}
+
+const initialState: AppState = {
+  position: "qb",
+  searchTerm: "",
+  mode: "light",
+};
 
 function App() {
-  const [position, setPosition] = useState<string>("qb");
-  const [searchTerm, setSearchTerm] = useState<string>("");
-  const [mode, setMode] = useState<string>("light");
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { position, searchTerm, mode } = state;
+
+  const updatePosition = function (position: string) {
+    dispatch({ type: "updatePosition", payload: position });
+  };
+
+  const updateSearchTerm = function (searchTerm: string) {
+    dispatch({ type: "updateSearchTerm", payload: searchTerm });
+  };
+
+  const updateMode = function (e: React.ChangeEvent<HTMLInputElement>) {
+    dispatch({
+      type: "updateMode",
+      payload: e.target.checked ? "dark" : "light",
+    });
+  };
 
   return (
-    <ModeContext.Provider value={mode}>
-      <div className={`outer ${mode}`}>
-        <div className="inner">
-          <Header>
-            <h1 className="heading">Fantasy Rankings</h1>
-            <label htmlFor="mode">Dark mode</label>
-            <input
-              id="mode"
-              type="checkbox"
-              value={mode}
-              checked={mode === "dark"}
-              onChange={(e) => setMode(e.target.checked ? "dark" : "light")}
-            />
-          </Header>
-          <nav>
-            <PositionSelect
-              labelText="Position"
-              searchablePositions={searchablePositions}
-              position={position}
-              setPosition={setPosition}
-              searchTerm={searchTerm}
-              setSearchTerm={setSearchTerm}
-            />
-          </nav>
-          <main>
-            <PlayerList position={position} searchTerm={searchTerm} />
-          </main>
-        </div>
+    <div className={`outer ${mode}`}>
+      <div className="inner">
+        <Header>
+          <h1 className="heading">Fantasy Rankings</h1>
+          <label htmlFor="mode">Dark mode</label>
+          <input
+            id="mode"
+            type="checkbox"
+            value={mode}
+            checked={mode === "dark"}
+            onChange={updateMode}
+          />
+        </Header>
+        <nav>
+          <PositionSelect
+            labelText="Position"
+            searchablePositions={searchablePositions}
+            position={position}
+            setPosition={(position: string) => updatePosition(position)}
+            searchTerm={searchTerm}
+            setSearchTerm={(searchTerm: string) => updateSearchTerm(searchTerm)}
+          />
+        </nav>
+        <main>
+          <PlayerList position={position} searchTerm={searchTerm} />
+        </main>
       </div>
-    </ModeContext.Provider>
+    </div>
   );
 }
 
