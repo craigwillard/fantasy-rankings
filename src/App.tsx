99486557490380f7f96ea6f60c84cec35f5ed550
Craig Willard <craigwillard@gmail.com>
2024-09-20 09:13:18 -0400

    Fix label/field relationship for mode

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ function App() {
             <h1 className="heading">Fantasy Rankings</h1>
             <label htmlFor="mode">Change to dark mode</label>
             <input
+              id="mode"
               type="checkbox"
               value={mode}
               checked={mode === "dark"}
